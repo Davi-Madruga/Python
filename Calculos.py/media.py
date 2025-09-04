78bdca67ca7ccf42549d815420c2279cc556843e
DAVI FARIAS MADRUGA <davi.madruga@cs.unipe.edu.br>
2025-09-04 17:39:31 -0300

    Alterações

--- Calculos.py/media.py
+++ Calculos.py/media.py
@@ -3,4 +3,4 @@ nota2 = int(input("Digite a nota2: "))#5
 nota3 = int(input("Digite a nota3: "))#6
 resultado = ((nota1*2)+(nota2*5)+(nota3*6)) / 13
 
-print(f"A média foi {resultado}")
+print(f"A média foi {resultado:.2f}")
---
+++ principios/Listas.py
@@ -0,0 +1,9 @@
+
+lista_instrutores = ["Augusto","Kaio","Uira"]
+lista_instrutores.clear()
+lista_instrutores.append("Rafael")
+lista_instrutores.insert(1,"Davi")
+lista_instrutores.insert(0,input("Adicione algum nome"))
+lista_instrutores.remove("Davi")
+lista_instrutores.pop()
+print(lista_instrutores)
