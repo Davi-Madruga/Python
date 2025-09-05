b20b47fe943e731648e4607a21fedfc878dacef4
DAVI FARIAS MADRUGA <davi.madruga@cs.unipe.edu.br>
2025-09-05 16:40:01 -0300

    Dicionarios em Python

---
+++ principios/Dictionary.py
@@ -0,0 +1,15 @@
+#Dicionário
+pessoa1 = {
+    "nome" : "Davi",
+    10 : "dez",
+}
+pessoa2 = {
+    "nome" : "Uira",
+}
+pessoas = [pessoa1,pessoa2]
+pessoa1
+
+for person in pessoas:
+    print(person)
+
+#print(pessoa1[10])
