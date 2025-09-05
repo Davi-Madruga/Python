856e23d42df27d088e6e7fbd6e7d19aec37cfb7b
DAVI FARIAS MADRUGA <davi.madruga@cs.unipe.edu.br>
2025-09-05 16:43:14 -0300

    Funções em Python

---
+++ principios/Func.py
@@ -0,0 +1,12 @@
+#Funções: def <nome da função>(<parametro>):
+"""def saudacao(nome):
+print(f"Olá {nome}")
+
+saudacao("carl")"""
+
+def soma(numero1,numero2):
+    resultado = numero1 + numero2
+    return resultado
+
+numero = soma(10,2)
+print(numero)
